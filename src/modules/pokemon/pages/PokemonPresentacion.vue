2fbc7bda7e79b4cb9358a164064adfa75ada7dac
Henry <henryivang@gmail.com>
2025-06-25 13:42:24 -0600

    style(componente-mosaico):ajusta tamaños de imagenes de mosaico

--- src/modules/pokemon/pages/PokemonPresentacion.vue
+++ src/modules/pokemon/pages/PokemonPresentacion.vue
@@ -2,7 +2,7 @@
 <section class="flex w-full items-center justify-center">
     <div class="hidden lg:grid grid-cols-4 w-[60%] grid-rows-4 p-10 h-full m-15 items-start">
         <div class="relative flex flex-col bg-white col-start-1 row-start-1 col-span-4 overflow-hidden">
-            <img class="object-cover object-center w-full h-[290px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500 " src="/src/assets/img/imagen_4.jpg">
+            <img class="object-cover object-center w-full h-[310px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500 " src="/src/assets/img/imagen_4.jpg">
               <div class="absolute top-0 right-0 p-4 text-white italic">
                 <h2>
                     New York, EEUU
@@ -11,36 +11,36 @@
         </div>
 
         <div class="flex flex-col bg-white col-start-2 row-start-2 row-span-2 col-end-4 overflow-hidden">
-            <video class="object-cover object-center w-full h-[295px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" controls autoplay muted loop>
+            <video class="object-cover object-center w-full h-[310px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" controls autoplay muted loop>
                 <source src="/src/assets/videos/video_1.mp4" type="video/mp4">
                 Tu navegador no soporta el video.
             </video>
         </div>
 
         <div class="flex flex-col bg-white col-start-1 row-start-2 col-end-1 row-span-3 overflow-hidden">
-            <img class="object-cover object-center w-full h-[290px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_5.jpg">
+            <img class="object-cover object-center w-full h-[310px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_5.jpg">
         </div>
 
         <div class="flex flex-col bg-white col-start-4 row-start-2 col-end-4 overflow-hidden">
-            <img class="object-cover object-center w-full h-[290px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_8.jpg">
+            <img class="object-cover object-center w-full h-[310px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_8.jpg">
         </div>
 
         <div class="relative flex flex-col bg-white col-start-1 row-start-3 col-span-4 overflow-hidden">
-           <img class="object-cover object-center w-full h-[290px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_10.jpg">
+           <img class="object-cover object-center w-full h-[310px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_10.jpg">
                 <div class="absolute top-0 right-0 p-4 text-black italic">
                     Honolulu, EEUU
                 </div>
         </div>
 
         <div class="flex flex-col bg-white col-start-3 row-start-4 col-span-3 overflow-hidden">
-           <video class="object-cover object-center w-full h-[195px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" controls autoplay muted loop>
+           <video class="object-cover object-center w-full h-[310px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" controls autoplay muted loop>
                <source src="/src/assets/videos/video_2.mp4" type="video/mp4">
                Tu navegador no soporta el video.
            </video>
         </div>
         
         <div class="flex flex-col bg-white col-start-1 row-start-4 col-span-2 overflow-hidden">
-           <img class="object-cover object-center w-full h-[195px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_12.jpg">
+           <img class="object-cover object-center w-full h-[310px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_12.jpg">
         </div>
     </div>
 
--- src/modules/pokemon/pages/PokemonPrueba.vue
+++ src/modules/pokemon/pages/PokemonPrueba.vue
@@ -2,7 +2,7 @@
 <section class="flex w-full items-center justify-center">
     <div class="hidden lg:grid grid-cols-4 w-[60%] grid-rows-4 p-10 h-full m-15 items-start">
         <div class="relative flex flex-col bg-white col-start-1 row-start-1 col-span-4 overflow-hidden">
-            <img class="object-cover object-center w-full h-[290px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500 " src="/src/assets/img/imagen_4.jpg">
+            <img class="object-cover object-center w-full h-[310px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500 " src="/src/assets/img/imagen_4.jpg">
               <div class="absolute top-0 right-0 p-4 text-white italic">
                 <h2>
                     New York, EEUU
@@ -11,30 +11,30 @@
         </div>
 
         <div class="flex flex-col bg-white col-start-2 row-start-2 row-span-2 col-end-4 overflow-hidden">
-            <img class="object-cover object-center w-full h-[295px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500"  src="/src/assets/img/imagen_7.jpg">
+            <img class="object-cover object-center w-full h-[310px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500"  src="/src/assets/img/imagen_7.jpg">
         </div>
 
         <div class="flex flex-col bg-white col-start-1 row-start-2 col-end-1 row-span-3 overflow-hidden">
-            <img class="object-cover object-center w-full h-[290px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_5.jpg">
+            <img class="object-cover object-center w-full h-[310px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_5.jpg">
         </div>
 
         <div class="flex flex-col bg-white col-start-4 row-start-2 col-end-4 overflow-hidden">
-            <img class="object-cover object-center w-full h-[290px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_8.jpg">
+            <img class="object-cover object-center w-full h-[310px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_8.jpg">
         </div>
 
         <div class="relative flex flex-col bg-white col-start-1 row-start-3 col-span-4 overflow-hidden">
-           <img class="object-cover object-center w-full h-[290px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_10.jpg">
+           <img class="object-cover object-center w-full h-[310px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_10.jpg">
                 <div class="absolute top-0 right-0 p-4 text-black italic">
                     Honolulu, EEUU
                 </div>
         </div>
 
         <div class="flex flex-col bg-white col-start-3 row-start-4 col-span-3 overflow-hidden">
-           <img class="object-cover object-center w-full h-[195px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_11.jpg">
+           <img class="object-cover object-center w-full h-[310px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_11.jpg">
         </div>
         
         <div class="flex flex-col bg-white col-start-1 row-start-4 col-span-2 overflow-hidden">
-           <img class="object-cover object-center w-full h-[195px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_12.jpg">
+           <img class="object-cover object-center w-full h-[310px] scale-110 transition-transform duration-500 ease-in-out hover:scale-100 border-solid border-4 border-gray-500" src="/src/assets/img/imagen_12.jpg">
         </div>
     </div>
 
